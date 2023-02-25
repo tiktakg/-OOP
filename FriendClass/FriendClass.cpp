fe589a6b918256e1ecd2bf63761808b43957c706
С Г <guplik@bk.ru>
2023-02-25 08:38:37 +0300

    уточнить как именно создается дружественная функция к обоим классам

--- FriendClass/FriendClass.cpp
+++ FriendClass/FriendClass.cpp
@@ -1,7 +1,182 @@
 ﻿#include <iostream>
+#include <string.h>
+#include <Windows.h>
+#include <conio.h>
+
+
+class Triandgle
+{
+private:
+	char cc;
+
+	class Point;
+	friend void gotoxy(Triandgle*, Point*);
+
+	
+
+public:
+	short x, y;
+
+	void charlocus(short X, short Y,char CC)
+	{
+		x = X;
+		y = Y;
+		cc = CC;
+	}
+
+	void display()
+	{
+		
+		std::cout << cc;
+	}
+
+	
+	
+};
+
+//void friendPutChar(Triandgle* p, char c)
+//{
+//	p->cc = c;
+//}
+
+void gotoxy(Triandgle* t, Point p)
+{
+	HANDLE stdOut = GetStdHandle(STD_OUTPUT_HANDLE);
+	COORD coord = { p->x,p->y };
+	COORD coord = { t->x,t->y };
+	SetConsoleCursorPosition(stdOut, coord);
+}
+
+
+class Point
+{
+private:
+	
+	char cc;
+
+	class Triandgle;
+	//friend void friendPutChar(Triandgle*, char);
+
+	friend void gotoxy(Point*,Triandgle*);
+
+	//void gotoxy(int x, int y)
+	//{
+	//	HANDLE stdOut = GetStdHandle(STD_OUTPUT_HANDLE);
+	//	COORD coord = { x,y };
+	//	SetConsoleCursorPosition(stdOut, coord);
+	//}
+
+public:
+	short x, y;
+
+	void charlocus(short X, short Y, char CC)
+	{
+		x = X;
+		y = Y;
+		cc = CC;
+	}
+
+	void display()
+	{
+		std::cout << cc;
+	}
+		
+
+};
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	Triandgle t;
+	Point p;
+	t.charlocus(3, 5, 'd');
+	p.charlocus(5, 3, 'g');
+	gotoxy(&t);\
+	gotoxy(&p);
+	t.display();
+	p.display();
+	
+
 }
 
+//#include <iostream>
+//#include <string.h>
+//#include <Windows.h>
+//#include <conio.h>
+//
+//
+//class Triandgle
+//{
+//private:
+//	int x, y;
+//	char cc;
+//
+//	friend void gotoxy(Triandgle*, int x, int y);
+//
+//
+//public:
+//	void charlocus(int X, int Y, char CC)
+//	{
+//		x = X;
+//		y = Y;
+//		cc = CC;
+//	}
+//
+//	void display()
+//	{
+//		std::cout << cc;
+//	}
+//
+//};
+//
+//void gotoxy(Triandgle* p, int x, int y)
+//{
+//	HANDLE stdOut = GetStdHandle(STD_OUTPUT_HANDLE);
+//	COORD coord = { x,y };
+//	SetConsoleCursorPosition(stdOut, coord);
+//}
+//
+//
+//
+//class Point
+//{
+//private:
+//	int x, y;
+//	char cc;
+//
+//	friend void gotoxy(Point*, int x, int y);
+//
+//
+//public:
+//	void charlocus(int X, int Y, char CC)
+//	{
+//		x = X;
+//		y = Y;
+//		cc = CC;
+//	}
+//
+//	void display()
+//	{
+//
+//		std::cout << cc;
+//	}
+//
+//
+//};
+//
+//
+//int main()
+//{
+//	Point t;
+//	Triandgle d;
+//
+//	t.charlocus(3, 5, 'd');
+//	d.charlocus(1, 5, 'd');
+//
+//
+//	gotoxy(&t, 6, 5);
+//	gotoxy(&d, 3, 1);
+//	t.display();
+//	d.display();
+//
+//}
+//
--- ProductIncapsulation/ProductIncapsulation.cpp
+++ ProductIncapsulation/ProductIncapsulation.cpp
@@ -1,8 +1,52 @@
 ﻿#include <iostream>
+#include <string.h>
+#include <Windows.h>
+
+
+class Product
+{
+private:
+	static float margin;
+
+
+	float price;
+	std::string name;
+
+public:
+	Product(std::string _name, float _price)
+	{
+		name = _name;
+		price = _price + margin;
+	}
+	~Product()
+	{
+		std::cout << "Product is delete. \n";
+	}
+
+	void ShowDate()
+	{
+		std::cout << "Name of porduct : " << name << std::endl;
+		std::cout << "price of porduct : " << price << std::endl;
+
+	}
+
+
+
+};
+
+  
+float Product::margin = 10;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+
+
+	Product product1("Potato", 10);
+	Product product2("Tomato", 15);
+
+	product1.ShowDate();
+	product2.ShowDate();
+
 }
 
 
