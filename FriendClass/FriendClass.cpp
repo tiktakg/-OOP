24ed3358f7b54f3fd4bc6452406223d744fa7114
gupli <gupli@DESKTOP-1VEJR2M>
2023-03-11 16:50:19 +0300

    Все сделанно

--- FriendClass/FriendClass.cpp
+++ FriendClass/FriendClass.cpp
@@ -4,7 +4,7 @@
 #include <conio.h>
 
 
-void gotoxy(float x, float y)
+void gotoxy(short x, short y)
 {
 	HANDLE stdOut = GetStdHandle(STD_OUTPUT_HANDLE);
 	COORD coord = { x,y };
@@ -15,40 +15,44 @@ class Triandgle
 {
 private:
 	char cc;
+	short x, y,x1,y1,x2,y2;
 
-	friend void gotoxy();
 
-	
+	friend void gotoxy(short x, short y);
 
 public:
-	short x, y;
 
-	void charlocus(short X, short Y,char CC)
+	void charlocus(short X, short Y, short X1, short Y1, short X2, short Y2, char CC)
 	{
 		x = X;
 		y = Y;
+		x1 = X1;
+		y1 = Y1;
+		x2 = X2;
+		y2 = Y2;
 		cc = CC;
 	}
 
 	void display()
 	{
-		
+		gotoxy(x, y);
+		std::cout << cc;
+		gotoxy(x1, y1);
+		std::cout << cc;
+		gotoxy(x2, y2);
 		std::cout << cc;
 	}
 
-	
 };
 
 class Point
 {
 private:
-	
 	char cc;
-
-	friend void gotoxy();
-
-	
 	short x, y;
+
+	friend void gotoxy(short x, short y);
+
 
 public:
 
@@ -61,105 +65,28 @@ public:
 
 	void display()
 	{
+		gotoxy( x,y);
 		std::cout << cc;
 	}
-		
-
 };
+
+
+
 
 int main()
 {
-	Triandgle t;
 	Point p;
-	t.charlocus(3, 5, 'd');
-	p.charlocus(5, 3, 'g');
-	gotoxy(t.x,t.y);
-	gotoxy(p.x, p.y);
-	t.display();
-	p.display();
-	
+	Triandgle t;
 
+	p.charlocus(1, 1, 'q');
+	p.display();
+
+	p.charlocus(5, 10, 'e');
+	p.display();
+
+	t.charlocus(1, 4, 6, 7, 8, 2, 'f');
+	t.display();
+	system("pause");
 }
 
-//#include <iostream>
-//#include <string.h>
-//#include <Windows.h>
-//#include <conio.h>
-//
-//
-//class Triandgle
-//{
-//private:
-//	int x, y;
-//	char cc;
-//
-//	friend void gotoxy(Triandgle*, int x, int y);
-//
-//
-//public:
-//	void charlocus(int X, int Y, char CC)
-//	{
-//		x = X;
-//		y = Y;
-//		cc = CC;
-//	}
-//
-//	void display()
-//	{
-//		std::cout << cc;
-//	}
-//
-//};
-//
-//void gotoxy(Triandgle* p, int x, int y)
-//{
-//	HANDLE stdOut = GetStdHandle(STD_OUTPUT_HANDLE);
-//	COORD coord = { x,y };
-//	SetConsoleCursorPosition(stdOut, coord);
-//}
-//
-//
-//
-//class Point
-//{
-//private:
-//	int x, y;
-//	char cc;
-//
-//	friend void gotoxy(Point*, int x, int y);
-//
-//
-//public:
-//	void charlocus(int X, int Y, char CC)
-//	{
-//		x = X;
-//		y = Y;
-//		cc = CC;
-//	}
-//
-//	void display()
-//	{
-//
-//		std::cout << cc;
-//	}
-//
-//
-//};
-//
-//
-//int main()
-//{
-//	Point t;
-//	Triandgle d;
-//
-//	t.charlocus(3, 5, 'd');
-//	d.charlocus(1, 5, 'd');
-//
-//
-//	gotoxy(&t, 6, 5);
-//	gotoxy(&d, 3, 1);
-//	t.display();
-//	d.display();
-//
-//}
-//
+
