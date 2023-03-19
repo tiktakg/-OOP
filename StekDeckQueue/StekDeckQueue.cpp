b905d54ff6000842bf90e55cabcccf8ce5ca5ce8
gupli <gupli@DESKTOP-1VEJR2M>
2023-03-19 18:27:37 +0300

    Полностью сделанн stack

--- StekDeckQueue/StekDeckQueue.cpp
+++ StekDeckQueue/StekDeckQueue.cpp
@@ -4,6 +4,127 @@
 
 
 
+std::string enterData(std::string text)
+{
+	std::string data;
+	std::cout << text;
+	std::cin >> data;
+	return data;
+}
+
+//struct Stack
+//{
+//private:
+//
+//	struct Node
+//	{
+//		std::string str;
+//		Node* next;
+//
+//		Node(std::string _str):next(nullptr)
+//		{
+//			str = _str;
+//		}
+//	};
+//
+//	Node* lastNode = nullptr;
+//
+//	void enterNode(std::string _str)
+//	{
+//		Node* node = new Node(_str);
+//
+//		if (lastNode != nullptr)
+//			node->next = lastNode;
+//
+//		lastNode = node;
+//		
+//	}
+//
+//	
+//	void deleteLastNode()
+//	{
+//		if (lastNode != nullptr)
+//		{
+//			Node*  _node = lastNode->next;
+//			delete(lastNode);
+//			lastNode = _node;
+//		}
+//		else
+//			std::cout << "List is note created\n";
+//	
+//	}
+//
+//	void printAndDeleteAllList()
+//	{
+//		if (lastNode == nullptr)
+//			std::cout << "List is note created\n";
+//
+//		while (lastNode != nullptr)
+//		{
+//			Node* _node = lastNode->next;
+//			std::cout << lastNode->str << std::endl;
+//			delete(lastNode);
+//			lastNode = _node;
+//		}
+//	}
+//
+//	void PrintLastNode()
+//	{
+//		
+//		if (lastNode != nullptr)
+//			std::cout << lastNode->str << std::endl;
+//		else
+//			std::cout << "List is note created\n";
+//		
+//	}
+//
+//public:
+//	void menu(Stack list)
+//	{
+//		int number;
+//		std::string data, bettenData;
+//
+//
+//		system("cls");
+//
+//		std::cout << "Choose a number:\n";
+//		std::cout << "1 - enter a node to end of list or create list\n";
+//		std::cout << "2 - Print first node \n";
+//		std::cout << "3 - delete first node\n";
+//		std::cout << "4 - Print and delete all node  \n";
+//		std::cout << "5 - exit\n";
+//
+//		std::cin >> number;
+//
+//		switch (number)
+//		{
+//		case 1:
+//			list.enterNode(enterData("Write a date for node\n"));
+//			break;
+//
+//		case 2:
+//			list.PrintLastNode();
+//			break;
+//
+//		case 3:
+//			list.deleteLastNode();
+//			break;
+//
+//		case 4:
+//			list.printAndDeleteAllList();
+//			break;
+//
+//		case 5:
+//			return;
+//			break;
+//		}
+//
+//		system("Pause");
+//		menu(list);
+//	}
+//};
+
+
 struct Queue
 {
 private:
@@ -13,70 +134,75 @@ private:
 		std::string str;
 		Node* next;
 
-		Node(std::string _str):next(nullptr)
+		Node(std::string _str) :next(nullptr)
 		{
 			str = _str;
 		}
 	};
 
+	Node* firstNode = nullptr;
 	Node* lastNode = nullptr;
 
 	void enterNode(std::string _str)
 	{
 		Node* node = new Node(_str);
 
-		if (lastNode != nullptr)
-			node->next = lastNode;
+		if (firstNode == nullptr)
+		{
+			firstNode = node;
+			return;
+		}
 
+		if (firstNode->next == nullptr)
+		{
+			firstNode->next = node;
+			lastNode = node;
+			return;
+		}
+
+		lastNode->next = node;
 		lastNode = node;
 		
 	}
 
-	
+
 	void deleteLastNode()
 	{
-		if (lastNode != nullptr)
+		if (firstNode != nullptr)
 		{
-			Node*  _node = lastNode->next;
-			delete(lastNode);
-			lastNode = _node;
+			Node* node = firstNode->next;
+			delete(firstNode);
+			firstNode = node;
 		}
 		else
 			std::cout << "List is note created\n";
-	
+
 	}
 
 	void printAndDeleteAllList()
 	{
-		if (lastNode == nullptr)
+		if (firstNode == nullptr)
 			std::cout << "List is note created\n";
 
-		while (lastNode != nullptr)
+		while (firstNode != nullptr)
 		{
-			Node* _node = lastNode->next;
-			std::cout << lastNode->str << std::endl;
-			delete(lastNode);
-			lastNode = _node;
+			Node* _node = firstNode->next;
+			std::cout << firstNode->str << std::endl;
+			delete(firstNode);
+			firstNode = _node;
 		}
 	}
 
 	void PrintLastNode()
 	{
-		
-		if (lastNode != nullptr)
-			std::cout << lastNode->str << std::endl;
+
+		if (firstNode != nullptr)
+			std::cout << firstNode->str << std::endl;
 		else
 			std::cout << "List is note created\n";
-		
 	}
 
-	std::string enterData(std::string text)
-	{
-		std::string data;
-		std::cout << text;
-		std::cin >> data;
-		return data;
-	}
+
 
 public:
 	void menu(Queue list)
@@ -89,9 +215,9 @@ public:
 
 		std::cout << "Choose a number:\n";
 		std::cout << "1 - enter a node to end of list or create list\n";
-		std::cout << "2 - Print last node \n";
-		std::cout << "3 - delete last node\n";
-		std::cout << "4 - Print all node and delete \n";
+		std::cout << "2 - Print first node \n";
+		std::cout << "3 - delete first node\n";
+		std::cout << "4 - Print and delete all node  \n";
 		std::cout << "5 - exit\n";
 
 		std::cin >> number;
@@ -123,7 +249,6 @@ public:
 		menu(list);
 	}
 };
-
 
 //struct Deck
 //{
@@ -362,240 +487,6 @@ public:
 //};
 //	}
 ////
-
-
-struct Stack
-{
-	struct node
-	{
-		std::string str;
-		node* next;
-
-		node(std::string _str)
-		{
-			str = _str;
-			next = nullptr;
-		}
-	};
-
-	node* last = nullptr;
-	node* first;
-
-
-	void enterNode(std::string _str)
-	{
-		node* _node = new node(_str);
-
-		if (last == nullptr)
-		{
-			last = _node;
-			first = _node;
-			return;
-		}
-		last = _node;
-		first->next = _node;
-		first = _node;
-
-	}
-
-	void addNode(std::string _str, std::string checkstr)
-	{
-		node* newNode = new node(_str);
-		node* firstNode = last;
-		node* nowNode = nullptr;
-
-		if (last == nullptr)
-		{
-			std::cout << "List is note created\n";
-			return;
-		}
-		else if (checkstr == firstNode->str)
-		{
-			last = newNode;
-			newNode->next = firstNode;
-			return;
-		}
-
-
-		while (firstNode->next != nullptr)
-		{
-			nowNode = firstNode->next;
-
-			if (checkstr == nowNode->str)
-			{
-				firstNode->next = newNode;
-				newNode->next = nowNode;
-				break;
-			}
-
-			firstNode = firstNode->next;
-		}
-	}
-
-	void deleteNode(std::string _str)
-	{
-		node* firstNode = last;
-		node* nowNode = nullptr;
-		node* lastNode = nullptr;
-
-		if (last == nullptr)
-		{
-			std::cout << "List is note created\n";
-			return;
-		}
-		else if (_str == firstNode->str)
-		{
-			nowNode = firstNode->next;
-			last = nowNode;
-			delete(firstNode);
-			return;
-		}
-
-		while (firstNode != nullptr)
-		{
-			nowNode = firstNode->next;
-			lastNode = nowNode->next;
-
-			if (_str == nowNode->str)
-			{
-				delete(nowNode);
-				firstNode->next = lastNode;
-				break;
-			}
-			else if (_str == lastNode->str and lastNode->next == nullptr)
-			{
-				nowNode->next = nullptr;
-				delete(lastNode);
-				break;
-			}
-
-
-			firstNode = firstNode->next;
-		}
-
-
-
-	}
-
-	void deleteList()
-	{
-		node* headNode = last;
-
-		if (last == nullptr)
-		{
-			std::cout << "List is note created\n";
-			return;
-		}
-
-		while (headNode != nullptr)
-		{
-			node* nowNode = headNode;
-			headNode = headNode->next;
-			delete(nowNode);
-		}
-		last = nullptr;
-	}
-
-	void printList()
-	{
-
-		node* _node = last;
-
-		if (last == nullptr)
-		{
-			std::cout << "List is empty\n";
-			return;
-		}
-
-		while (_node != nullptr)
-		{
-			std::cout << _node->str << std::endl;
-
-			_node = _node->next;
-		}
-	}
-
-
-	void checkNode(std::string _str)
-	{
-		node* node = last;
-
-		if (last == nullptr)
-		{
-			std::cout << "List is note created\n";
-			return;
-		}
-
-		while (node != nullptr)
-		{
-			if (_str == node->str)
-				std::cout << node->str << std::endl;
-
-			node = node->next;
-		}
-	}
-
-	std::string enterData(std::string text)
-	{
-		std::string data;
-		std::cout << text;
-		std::cin >> data;
-		return data;
-	}
-
-	void menu(Stack list)
-	{
-		int number;
-		std::string data, bettenData;
-
-
-		system("cls");
-
-		std::cout << "Choose a number:\n";
-		std::cout << "1 - enter a node to end of list or create list\n";
-		std::cout << "3 - delete a node\n";
-		std::cout << "4 - check a node\n";
-		std::cout << "5 - check all node\n";
-		std::cout << "6 - delete all node\n";
-		std::cout << "7 - exit\n";
-
-		std::cin >> number;
-
-
-
-		switch (number)
-		{
-		case 1:
-			list.enterNode(enterData("Write a date for node\n"));
-			break;
-
-		case 3:
-			list.deleteNode(enterData("Write a node for delete\n"));
-			break;
-
-		case 4:
-			list.checkNode(enterData("Write node for check\n"));
-			break;
-
-		case 5:
-			list.printList();
-			break;
-
-		case 6:
-			list.deleteList();
-			break;
-
-		case 7:
-			return;
-			break;
-		}
-
-		system("Pause");
-		menu(list);
-
-	}
-};
-
 
 
 
