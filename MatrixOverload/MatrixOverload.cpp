6a3d2a3213b41a8fea6d569d561f36ad5d445844
gupli <gupli@DESKTOP-1VEJR2M>
2023-03-19 21:08:46 +0300

    Начата работа над матрицой

---
+++ MatrixOverload/MatrixOverload.cpp
@@ -0,0 +1,34 @@
+#include "iostream"
+#include "windows.h"
+
+
+
+
+class Matrix
+{
+	int lengthN;
+	int lengthM;
+	int matrix;
+
+	Matrix(int LengthN,int LengtM)
+	{
+		lengthN = LengthN;
+	}
+	~Matrix()
+	{
+
+	}
+
+	void DisplayMatrix()
+	{
+
+	}
+	
+};
+
+
+
+Matrix* operator + (Matrix MatrixA, Matrix MatriB)
+{
+
+}
