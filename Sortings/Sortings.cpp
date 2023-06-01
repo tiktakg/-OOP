04eddd25acd750c26a0f2d48a3028ce0bffd64cd
gupli <gupli@DESKTOP-1VEJR2M>
2023-06-01 17:08:30 +0300

    Начата реализация структур данных

--- Sortings/Sortings.cpp
+++ Sortings/Sortings.cpp
@@ -1,20 +1,194 @@
-﻿// Sortings.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include <iostream>
 
-#include <iostream>
+#define showText(text) std::cout << std::endl << text << std::endl;
+#define showLine std::cout << "--------------------------" << std::endl;
+#define size 10
+
+void showArr();
+void copyArr();
+
+void sortInsertion();
+void sortGnomes();
+void sortShell();
+void sortHora();
+void sortLSD();
+void sortMSD();
+void sortTimsort();
+
+int arr[size];
+int defaultArr[size] = { 1,8,3,7,4,15,5,-1,34,-6 };
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    showArr();
+    showLine
+        sortInsertion();
+
+
+    showArr();
+    showLine
+        sortGnomes();
+
+    showArr();
+    showLine
+        sortShell();
+
+    showArr();
+    showLine
+        sortHora();
+
+    showArr();
+    showLine
+        sortLSD();
+
+    showArr();
+    showLine
+        sortMSD();
+
+    showArr();
+    showLine
+        sortTimsort();
+    showArr();
+    showLine
+
 }
 
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+void copyArr()
+{
+    for (int i = 0; i < size; ++i)
+        arr[i] = defaultArr[i];
+}
 
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
+void showArr()
+{
+    for (int i = 0; i < size; ++i)
+        std::cout << arr[i] << " ";
+
+    std::cout << "\n";
+
+}
+
+void sortInsertion()
+{
+    copyArr();
+    showText("Sort insertion")
+
+        for (int i = 1; i < size; i++)
+            for (int j = i; j > 0 && arr[j - 1] > arr[j]; j--) // пока j>0 и элемент j-1 > j, x-массив int
+                std::swap(arr[j - 1], arr[j]);
+
+    showArr();
+    copyArr();
+}
+
+void sortGnomes()
+{
+    showText("Sort gnomes")
+
+        int i = 0;
+    while (i < size)
+    {
+        if (i == 0 || arr[i - 1] <= arr[i])
+            ++i;
+        else {
+            int tmp = arr[i];
+            arr[i] = arr[i - 1];
+            arr[i - 1] = tmp;
+            --i;
+        }
+    }
+
+    showArr();
+    copyArr();
+}
+
+void sortShell()
+{
+    showText("Sort shell")
+
+        int i, j, step;
+    int tmp;
+    for (step = size / 2; step > 0; step /= 2)
+        for (i = step; i < size; i++)
+        {
+            tmp = arr[i];
+            for (j = i; j >= step; j -= step)
+            {
+                if (tmp < arr[j - step])
+                    arr[j] = arr[j - step];
+                else
+                    break;
+            }
+            arr[j] = tmp;
+        }
+
+    showArr();
+    copyArr();
+}
+
+void sortHora()
+{
+    showText("Sort hora");
+
+    void hoarasort(int* a, int first, int last);
+    hoarasort(arr, 0, size - 1);
+
+    showArr();
+    copyArr();
+}
+
+void sortLSD()
+{
+    showText("Sort LSD")
+
+        showArr();
+    copyArr();
+}
+
+void sortMSD()
+{
+    showText("Sort MSD")
+
+        showArr();
+    copyArr();
+}
+
+void sortTimsort()
+{
+    showText("Sort timsort")
+
+        showArr();
+    copyArr();
+}
+
+void hoarasort(int* a, int first, int last)
+{
+
+    int i = first, j = last;
+    double tmp, x = a[(first + last) / 2];
+
+    do
+    {
+        while (a[i] < x)
+            i++;
+        while (a[j] > x)
+            j--;
+
+        if (i <= j)
+        {
+            if (i < j)
+            {
+                tmp = a[i];
+                a[i] = a[j];
+                a[j] = tmp;
+            }
+            i++;
+            j--;
+        }
+    } while (i <= j);
+
+    if (i < last)
+        hoarasort(a, i, last);
+    if (first < j)
+        hoarasort(a, first, j);
+}
---
+++ dataStruct/dataStruct.cpp
@@ -0,0 +1,264 @@
+﻿#include <iostream>
+#include <vector>
+#include <Windows.h>
+#include <list> 
+#include <set>
+
+
+#define showText(text, text2) std::cout << text << " " << text2 << std::endl
+#define pauseAndClear { \
+system("pause"); \
+system("cls");};
+
+void showVector();
+void showList();
+void showSet();
+void showMultiset();
+void showMap();
+void showStack();
+void showQueue();
+void showPriorityQueue();
+void showBitSet();
+void showUnorderedSet();
+
+
+
+int main()
+{
+	system("color 02");
+
+	showVector();
+	showList();
+	showSet();
+	showMultiset();
+	showMap();
+	showStack();
+	showQueue();
+	showPriorityQueue();
+	showBitSet();
+	showUnorderedSet();
+
+}
+
+void showVector()
+{
+
+	std::vector <int> vector;
+
+	showText("Work of vector", "");
+	showText(vector.size(), " - Size of empty vector");
+
+	vector.resize(2);
+
+	showText(vector.size(), " - Size of empty with resize vector");
+
+	for (const int i : vector)
+		showText(i, " - Elements of empty vector");
+
+	vector.push_back(1);
+	vector.push_back(2);
+	vector.push_back(3);
+
+	showText("--------------", "\nElements of vector after push back");
+
+	for (const int i : vector)
+		showText(i, "");
+
+	pauseAndClear;
+}
+
+void showList()
+{
+	std::list <int> list;
+
+	showText("Work of list", "");
+	showText(list.size(), " - Size of list");
+
+	list.resize(2);
+
+	showText(list.size(), " - Size after resize list");
+	showText("--------------", "");
+
+	for (const int i : list)
+		showText(i, " - Elements of empty list");
+
+	list.push_back(1);
+
+	showText("Elements of list after push back 1", "");
+
+	for (const int i : list)
+		showText(i, "");
+
+	list.push_front(2);
+
+	showText("Elements of list after push front 2", "");
+
+	for (const int i : list)
+		showText(i, "");
+
+	pauseAndClear;
+}
+
+template <typename T>
+
+#pragma region Set
+
+class customSet
+{
+public:
+	class Node
+	{
+	public:
+		T element;
+		Node* next;
+
+		Node(T _element) :next(nullptr)
+		{
+			element = _element;
+		}
+
+	};
+
+	Node* start = nullptr;
+	Node* last = nullptr;
+
+	void insert(T element)
+	{
+		if (!checkElement(element))
+			return;
+
+		Node* _node = new Node(element);
+
+		if (start == nullptr)
+		{
+			start = _node;
+			last = _node;
+		}
+		else
+		{
+			last->next = _node;
+			last = _node;
+		}
+	}
+
+	void showElement()
+	{
+		Node* _node = start;
+
+		while (_node != nullptr)
+		{
+			showText(_node->element, "");
+			_node = _node->next;
+		}
+	}
+
+	void erase(T element)
+	{
+		Node* _node = start;
+		Node* _last = nullptr;
+		Node* _next = nullptr;
+		while (_node != nullptr)
+		{
+			if (_node->element == element)
+			{
+				Node* _next = _node->next;
+				_last->next = _next;
+				delete(_node);
+				return;
+
+			}
+			_last = _node;
+			_node = _node->next;
+
+		}
+	}
+
+	bool checkElement(T element)
+	{
+		Node* _node = start;
+
+
+		while (_node != nullptr)
+		{
+			if (_node->element == element)
+				return false;
+
+			_node = _node->next;
+		}
+
+		return true;
+	}
+};
+
+void showSet()
+{
+	showText("Work of custom set", "");
+
+	customSet<int> customSet;
+
+	customSet.insert(1);
+	customSet.insert(2);
+	customSet.insert(3);
+	customSet.insert(4);
+	customSet.insert(5);
+
+	showText("Show element of customr set after inesrt 1 2 3 4 5", "");
+
+	customSet.showElement();
+
+	showText("--------------", "\nShow element of customr set after erase 3 ");
+
+	customSet.erase(3);
+	customSet.showElement();
+
+	pauseAndClear;
+}
+
+#pragma endregion
+
+
+
+void showMultiset()
+{
+	pauseAndClear;
+}
+
+void showMap()
+{
+	pauseAndClear;
+
+}
+
+void showStack()
+{
+	pauseAndClear;
+
+}
+
+void showQueue()
+{
+	pauseAndClear;
+
+}
+
+void showPriorityQueue()
+{
+	pauseAndClear;
+
+}
+
+void showBitSet()
+{
+	pauseAndClear;
+
+}
+
+void showUnorderedSet()
+{
+	pauseAndClear;
+
+}
+
+
+
+
